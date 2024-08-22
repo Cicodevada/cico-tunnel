844e2715be5f8e98f8776837f9909bf27df31e60
Hugo Santos Lisboa <hugohotelmobiles@gmail.com>
2024-08-22 12:10:10 -0300

    Change version
    Tinha esquecido

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cico-tunnel",
-    version="1.1.3",
+    version="1.1.4",
     packages=find_packages(),
     install_requires=[
         "colorama",
